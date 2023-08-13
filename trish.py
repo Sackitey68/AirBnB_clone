de1423f61cd92e0366f54a3944187b50b8b15a35
root <sackitey68@gmail.com>
2023-08-13 07:47:07 -0700

    Passes pycodestyle

--- trish.py
+++ trish.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-def calculate_mul(a,b):
-	" " "
-this function calculates the product of two numbers
 
-Args: 
-	a (int): The first number
-	b (int): The second number
-Return:
-	int: the product of the two numbers
-	" " "
-mul_value= a*b
-result mul_value
+print("A beautiful code that passes the pycodestyle check")
