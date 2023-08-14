1b87dddd3366b052ce478fbf3ae9fe7f9ff6bdbe
root <sackitey68@gmail.com>
2023-08-13 19:20:23 -0700

    Added state.py

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module creates a User class"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class for managing state instance"""
+
+    name = ""
